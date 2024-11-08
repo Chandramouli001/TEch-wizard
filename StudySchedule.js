67de74cbb654de55b2574c4d0e998508cef9ce1e
Chandramouli Haldar <148478906+Chandramouli001@users.noreply.github.com>
2024-11-08 21:31:14 +0530

    Create StudySchedule.js

---
+++ StudySchedule.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const StudySchedule = () => {
+  return (
+    <View>
+      <Text>Study Schedule</Text>
+      <Text>Math: 9:00 AM - 10:00 AM</Text>
+      <Text>English: 10:30 AM - 11:30 AM</Text>
+      <Text>Science: 1:00 PM - 2:00 PM</Text>
+    </View>
+  );
+};
+
+export default StudySchedule;
