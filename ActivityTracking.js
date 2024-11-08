afb652230e60982079e5327012c9e0f6a2e4a664
Chandramouli Haldar <148478906+Chandramouli001@users.noreply.github.com>
2024-11-08 21:31:25 +0530

    Create ActivityTracking.js

---
+++ ActivityTracking.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const ActivityTracking = () => {
+  return (
+    <View>
+      <Text>Screen Time: 3 hours</Text>
+      <Text>Physical Activity: 30 minutes outdoor walk</Text>
+      <Text>Offline Activity: 45 minutes reading</Text>
+    </View>
+  );
+};
+
+export default ActivityTracking;
